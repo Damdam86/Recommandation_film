b7b1dfbe5694c1bc4750848e7915972e6278616c
DamCo <73948784+Damdam86@users.noreply.github.com>
2024-11-20 14:29:02 +0100

    Add files via upload
    ..Avec les liens vers les nouvelles images.

--- page1.py
+++ page1.py
@@ -9,17 +9,16 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.header("Yosser")
-    st.image("https://github.com/Damdam86/Recommandation_film/blob/3da5a9e949623dc7be79b8cbdf51f17a80771178/images/Yosser.png")
+    st.image("https://raw.githubusercontent.com/Damdam86/Recommandation_film/main/images/Yosser.png")
 
 with col2:
     st.header("Vincent")
-    st.image("https://github.com/Damdam86/Recommandation_film/blob/3da5a9e949623dc7be79b8cbdf51f17a80771178/images/Vincent.png")
+    st.image("https://raw.githubusercontent.com/Damdam86/Recommandation_film/main/images/Vincent.png")
 
 with col3:
     st.header("Damien")
-    st.image("https://github.com/Damdam86/Recommandation_film/blob/3da5a9e949623dc7be79b8cbdf51f17a80771178/images/Damien.png")
+    st.image("https://raw.githubusercontent.com/Damdam86/Recommandation_film/main/images/Damien.png")
 
 with col4:
     st.header("Fatma")
-    st.image("https://github.com/Damdam86/Recommandation_film/blob/3da5a9e949623dc7be79b8cbdf51f17a80771178/images/Fatma.png")
-
+    st.image("https://raw.githubusercontent.com/Damdam86/Recommandation_film/main/images/Fatma.png")
